b94a1010c8d81d3073d6284b08e7a11a7810ef91
liyuxiao222 <89355060+liyuxiao2@users.noreply.github.com>
2023-07-24 12:07:35 -0400

    added a button adds one with every click

--- src/components/featurePage.js
+++ src/components/featurePage.js
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css'
 
-const Header = () => {
-    const getYear = () =>{
-        const day = new Date();
-        return day.getDate();
+class Header extends Component{
+
+    state = {
+        name: 'Francis',
+        title: 'website',
+        keywords:'',
+        count:0
     }
-    return(
-        <div className='App-link'>
-            Date: {getYear()}
-        </div>
-    ) 
+
+    inputNameHandler = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            keywords:''
+        })
+    }
+
+    addOne(){
+        this.setState((state, props)=>({
+            count: state.count + 1
+        }))
+    }
+
+    getYear(){
+        const date = new Date();
+        let day = date.getDate()
+        let month = date.getMonth()
+        let year = date.getFullYear()
+        return day+ "/" + month + "/" + year;
+    }
+
+    render(){
+
+        
+        return(
+            <header>
+                <div className='App-link'> Date {this.getYear()} </div>
+
+                <br/>
+
+                <div>Enter your name </div>
+
+                <input  className='App'
+                    onChange={this.inputNameHandler}
+                />
+
+                <div> {this.state.count} </div>
+                <button onClick={ ()=> this.addOne() } >click on me</button>
+            </header>   
+        ) 
+    }
 }
 
 export default Header
