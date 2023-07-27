96ef0b58fe61911eb13c8e9c595410d6d28d6330
liyuxiao222 <89355060+liyuxiao2@users.noreply.github.com>
2023-07-27 13:26:55 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import './components/App.css';
 import Header from './components/featurePage';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTE1jo-MFUzwEjJfKjNXTKw4XuAayfms8",
+  authDomain: "liyuxiao-s-website.firebaseapp.com",
+  projectId: "liyuxiao-s-website",
+  storageBucket: "liyuxiao-s-website.appspot.com",
+  messagingSenderId: "883583350985",
+  appId: "1:883583350985:web:c171d3e322bf007ecdb518",
+  measurementId: "G-1ZNC99PSSM"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
