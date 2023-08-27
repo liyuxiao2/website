e1bacc3c50fd21d6ab16ee7a36c6d3bcc530a519
liyuxiao222 <89355060+liyuxiao2@users.noreply.github.com>
2023-08-27 15:29:57 -0400

    commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,15 @@
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import "./pages/CSS/App.css"
-import { BottomNavigation} from "@mui/material";
+import { BottomNavigation, ThemeProvider} from "@mui/material";
 import { BottomNavigationAction } from '@mui/material';
 import HomePage from './pages/homePage';
+
+const primary = {
+  main: '#1976d2',
+  light: '#42a5f5',
+  dark: '#1565c0',
+  contrastText: '#fff',
+};
 
 
 function Layout() {
@@ -11,10 +18,12 @@ function Layout() {
         <BottomNavigation 
             showLabels 
         >
-            <BottomNavigationAction label=  {<Link to = "Home" > Home </Link>}/>
-            <BottomNavigationAction label = {<Link to = "Header" > About me </Link>} />
-            <BottomNavigationAction label= {<Link to= "Contact" > Contact</Link> } />
-            <BottomNavigationAction label=  {<Link to = "Courses" > Courses </Link>}/>
+            <ThemeProvider  theme ={primary}>
+              <BottomNavigationAction label=  {<Link to = "Home" > Home </Link>}/>
+              <BottomNavigationAction label = {<Link to = "Header" > About me </Link>} />
+              <BottomNavigationAction label= {<Link to= "Contact" > Contact</Link> } />
+              <BottomNavigationAction label=  {<Link to = "Courses" > Courses </Link>}/>
+            </ThemeProvider>
             
         </BottomNavigation>
         </div>
--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import './CSS/App.css';
 import MyInfo from '../functions/myInfo';
+import Content from "/Users/liyuxiao/Documents/GitHub/website/src/components/functions/HomePage.json";
 
 class HomePage extends Component{
+    state = {
+        content: Content
+    }
     render(){
+
         return(
             <div className= 'App' >
-                hello
+                <MyInfo content = {this.state.content}/>
             </div>
         )
     }
